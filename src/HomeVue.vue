898b16906d83e2e6bed094f2635dba21d759eff5
alanawessling <wesslingalana@gmail.com>
2026-08-11 21:57:08 -0300

    home.vue pronto

---
+++ src/HomeVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+<template>
+    <div class="home">
+         
+        <h1>Coffee Quality Challenge</h1>
+
+          <p>Campeonato de Avaliação da Qualidade do Café </p> 
+
+          <button @click="$emit('pagAvaliacao')"> Avaliar Café </button> 
+
+    </div>
+</template>

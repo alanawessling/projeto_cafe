980c5a48852715d7be43ac42a4c8018e064f871e
alanawessling <wesslingalana@gmail.com>
2026-08-11 23:56:54 -0300

    style home

--- src/HomeVue.vue
+++ src/HomeVue.vue
@@ -12,4 +12,25 @@
 
     </div>
 </template>
-<style scoped></style>
+<style scoped>
+.home { 
+text-align: center; 
+padding: 70px 20px; 
+} 
+.home h1 { 
+font-size: 35px;
+color: #673411
+} 
+.home p { 
+font-size: 18px; 
+margin-bottom: 25px; 
+} 
+.home button { 
+background: #673411; 
+color: white; 
+border: none; 
+padding: 12px 20px; 
+margin: 5px; 
+border-radius: 6px; 
+}
+</style>
